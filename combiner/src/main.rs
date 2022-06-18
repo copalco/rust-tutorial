c4d67b06d4c6c689cf3870ad95a1d622c85db64f
PiotrKopalko <>
2022-06-18 20:34:18 +0000

    Completed task

--- combiner/src/main.rs
+++ combiner/src/main.rs
@@ -1,4 +1,4 @@
-// Lesson #65
+// Lesson #80
 #![allow(unused_variables, dead_code)]
 mod args;
 
@@ -7,7 +7,7 @@ use image::{
   imageops::FilterType::Triangle, io::Reader, DynamicImage, GenericImageView, ImageFormat,
 };
 
-fn main() -> Result<(), String> {
+fn main() -> Result<(), ImageDataErrors> {
   let args = Args::new();
   println!("{:?}", args);
 
@@ -15,13 +15,32 @@ fn main() -> Result<(), String> {
   let (image_2, image_2_format) = find_image_from_path(args.image_2);
 
   if image_1_format != image_2_format {
-    return Err(String::from("Image formats must match"));
+    return Err(ImageDataErrors::DifferentImageFormats);
   }
 
   let (image_1, image_2) = standardise_size(image_1, image_2);
-  let output = FloatingImage::new(image_1.width(), image_1.height(), args.output);
+  let mut output = FloatingImage::new(image_1.width(), image_1.height(), args.output);
 
+  let combined_data = combine_images(image_1, image_2);
+
+  output.set_data(combined_data)?;
+
+  image::save_buffer_with_format(
+    output.name,
+    &output.data,
+    output.width,
+    output.height,
+    image::ColorType::Rgba8,
+    image_1_format,
+  )
+  .unwrap();
   Ok(())
+}
+
+#[derive(Debug)]
+enum ImageDataErrors {
+  BufferTooSmall,
+  DifferentImageFormats,
 }
 
 struct FloatingImage {
@@ -41,6 +60,13 @@ impl FloatingImage {
       data: buffer,
       name,
     }
+  }
+  fn set_data(&mut self, data: Vec<u8>) -> Result<(), ImageDataErrors> {
+    if data.len() > self.data.capacity() {
+      return Err(ImageDataErrors::BufferTooSmall);
+    }
+    self.data = data;
+    Ok(())
   }
 }
 
@@ -70,21 +96,27 @@ fn get_smallest_dimensions(dim_1: (u32, u32), dim_2: (u32, u32)) -> (u32, u32) {
 fn combine_images(image_1: DynamicImage, image_2: DynamicImage) -> Vec<u8> {
   let vec_1 = image_1.to_rgba8().into_vec();
   let vec_2 = image_2.to_rgba8().into_vec();
-  vec_2
+
+  alternate_pixels(vec_1, vec_2)
 }
 
 fn alternate_pixels(vec_1: Vec<u8>, vec_2: Vec<u8>) -> Vec<u8> {
-  let combined_data = vec![0u8; vec_1.len()];
+  let mut combined_data = vec![0u8; vec_1.len()];
 
   let mut i = 0;
   while i < vec_1.len() {
+    if i % 8 == 0 {
+      combined_data.splice(i..=i + 3, set_rgba(&vec_1, i, i + 3));
+    } else {
+      combined_data.splice(i..=i + 3, set_rgba(&vec_2, i, i + 3));
+    }
     i += 4;
   }
 
   combined_data
 }
 
-fn set_rgba(vec: Vec<u8>, start: usize, end: usize) -> Vec<u8> {
+fn set_rgba(vec: &Vec<u8>, start: usize, end: usize) -> Vec<u8> {
   let mut rgba = Vec::new();
   for i in start..=end {
     let val = match vec.get(i) {
@@ -94,14 +126,5 @@ fn set_rgba(vec: Vec<u8>, start: usize, end: usize) -> Vec<u8> {
     rgba.push(val);
   }
   rgba
-}
-
-#[cfg(test)]
-mod tests {
-  use super::*;
-  #[test]
-  fn alternate_pixels_not_infinite() {
-    let _a = alternate_pixels(vec![1, 2, 3], vec![3, 2, 1]);
-  }
 }
 
